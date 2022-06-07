dfd2597df49fb5c6fa57f3c9e1be307e7dece8a7
WonXd <103139749+WonXd@users.noreply.github.com>
2022-06-08 04:01:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,11 +44,11 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/lord_ammar_quoteser'
-global.sgh = 'https://github.com/AmmarrBN'
-global.sgc = 'https://chat.whatsapp.com/BYcW7rue6fA8gNdyLPedDe'
+global.sig = 'https://instagram.com/sampeanikupicek'
+global.sgh = 'https://github.com/WonXd'
+global.sgc = 'https://chat.whatsapp.com/H19PmauFMTGL6HwQtQRTjW'
 global.sdc = '-'
-global.snh = 'https://youtube.com/channel/UCFeZ5BGt8lbOZwIj2MNOlIQ'
+global.snh = 'https://youtube.com/channel/UCsgSYvuFWqzt8NSP_DUVicQ'
 
 /*============== PAYMENT ==============*/
 global.pdana = '085712420674'
@@ -59,10 +59,10 @@ global.ppulsa2 = '088229683561'
 global.psaweria = 'https://saweria.com/kannabotz'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62882003127450'
+global.nomorbot = '6282141962028'
 global.nomorown = '62895337447795'
-global.namebot = 'Lord-Botz'
-global.nameown = 'Ammar-Executed'
+global.namebot = 'WonXd-Md'
+global.nameown = 'CaCantik X Arii'
 
 
 /*============== STAFF ==============*/
@@ -73,7 +73,7 @@ global.owner = [
   ['6288229683561', '⌬ Ammar Ganz', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = ['6288229683561'] // Want some help?
+global.mods = ['62895337447795'] // Want some help?
 global.prems = ['6288229683561','6287708773367'] // Premium user bukan disini nambahinnya, ketik .addprem @user 10
 
 
@@ -116,13 +116,13 @@ global.APIKeys = { // APIKey Here
 
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 Lord Botz ⁩㊣ 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Lord - Khun˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ Lord 𝗕𝗢𝗧'
+global.wm = 'WonXd' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Won - Khun˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ WonXd'
 global.botdate = `⫹⫺ 𝗗𝗮𝘁𝗲: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
 global.titlebot = `⫹⫺ RPG BOT Whatsapp | By ${global.nameown}`
-global.author = '                「 Lord BOT あ⁩ 」'
+global.author = '@sampeanikupicek'
 
 
 /*============== LOGO ==============*/
@@ -190,7 +190,7 @@ global.htjava = '⫹⫺' //hiasan Doang :v
 
 /*============== STICKER WM ==============*/
 global.stickpack = '.'
-global.stickauth = `☂︎ Lord Botz\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
+global.stickauth = `☂︎ WonXd Botz\n⫹⫺ Whatsapp BOT\nwa.me/${global.nomorbot}`
 
 global.multiplier = 38 // The higher, The harder levelup
 
